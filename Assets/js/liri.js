8b8dfbf3d2906207b33419fd6f49add7cd903e22
acher249 <32597382+acher249@users.noreply.github.com>
2018-08-24 18:48:57 -0400

    update
    update

--- Assets/js/liri.js
+++ Assets/js/liri.js
@@ -38,19 +38,18 @@ function spotifyThisSong(querySearch){
 
 function movieThis(querySearch){
 
-    omdb.search('Anchorman', function(err, movies) {
-      if(err) {
-          return console.error(err);
-      }
-  
-      if(movies.length < 1) {
-          return console.log('No movies were found!');
-      }
-  
-      movies.forEach(function(movie) {
-          console.log('%s (%d)', movie.title, movie.year);
-      });
-  });
+  omdb.get({ title: querySearch}, true, function(err, movie) {
+    if(err) {
+        return console.error(err);
+    }
+ 
+    if(!movie) {
+        return console.log('Movie not found!');
+    }
+ 
+    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+    console.log(movie.plot);
+});
 }
    
 if(firstArg === "spotifyThisSong"){
