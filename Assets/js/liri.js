4cb4cb9df848884e3044bc61e0a4a08a4722c00a
Adam <jchernick2010@gmail.com>
2018-08-25 13:05:55 -0400

    update

--- Assets/js/liri.js
+++ Assets/js/liri.js
@@ -4,13 +4,16 @@ require("dotenv").config();
 var keyFile = require("./key.js");
 
 var Spotify = require('node-spotify-api');
-var omdb = require('omdb');
+// var omdb = require('omdb');
+var omdbApi = require('omdb-client');
 
 var spotID = keyFile.spotify.id;
 var spotSecret = keyFile.spotify.secret;
 
-console.log("spotify ID: " + spotID);
-console.log("spotify secret: " + spotSecret);
+var omdbApiKey = "trilogy"
+
+// console.log("spotify ID: " + spotID);
+// console.log("spotify secret: " + spotSecret);
 
 // user enter commans line arguments to search the API
 var firstArg = process.argv[2];
@@ -27,35 +30,49 @@ var response;
 
 function spotifyThisSong(querySearch){
     spotify
-    .search({ type: 'artist', query: querySearch, limit: 1 }/*, callback*/)
+    .search({ type: 'track', query: querySearch, limit: 1 }/*, callback*/)
     .then(function(response) {
-      console.log(response.artists.items[0].external_urls);
+      console.log("Artist: " + response.tracks.items[0].artists[0].name);
+      console.log("Spotify Link: " + response.tracks.items[0].artists[0].external_urls.spotify);
     })
   .catch(function(err) {
     console.error('Error occurred: ' + err); 
   });
 }
 
+function spotifyThisArtist(querySearch){
+  spotify
+  .search({ type: 'artist', query: querySearch, limit: 1 }/*, callback*/)
+  .then(function(response) {
+    console.log("Followers: " + response.artists.items[0].followers.total);
+    console.log("Popularity: " + response.artists.items[0].popularity);
+    console.log("Spotify Link: " + response.artists.items[0].external_urls.spotify);
+  })
+.catch(function(err) {
+  console.error('Error occurred: ' + err); 
+});
+}
+
+//omdb parameters
+var params = {
+  apiKey: omdbApiKey,
+  query: querySearch,
+}
+
 function movieThis(querySearch){
 
-  omdb.get({ title: querySearch}, true, function(err, movie) {
-    if(err) {
-        return console.error(err);
-    }
- 
-    if(!movie) {
-        return console.log('Movie not found!');
-    }
- 
-    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
-    console.log(movie.plot);
-});
+  omdbApi.search(params, function(err, data) {
+    console.log(data);
+  });
 }
    
 if(firstArg === "spotifyThisSong"){
-  console.log("First Arg is Spotify");
+  // console.log("First Arg is Spotify - Pick Song");
   spotifyThisSong(querySearch);
-}else {
+}else if (firstArg === "spotifyThisArtist"){
+  // console.log("First Arg is Spotify - Pick Artist");
+  spotifyThisArtist(querySearch);
+}else{
   console.log("First Arg is Movie");
   movieThis(querySearch);
 };
